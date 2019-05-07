cbae4e3eb6b951c89b630f6c843b025c024634ff
JanPel <jan.pel@futurefacts.nl>
2019-05-07 11:59:09 +0200

    added a total_function that takes over looping from python

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 extern crate cpython;
 
 use cpython::{Python, PyResult};
-use std::collections::HashMap; 
+use std::collections::{HashMap,HashSet}; 
 
 fn calculate_haversine(mut lon : f64,mut lat: f64,mut lon_2: f64,mut lat_2: f64) -> f64 {
     lon = lon.to_radians();
@@ -17,12 +17,52 @@ fn calculate_haversine(mut lon : f64,mut lat: f64,mut lon_2: f64,mut lat_2: f64)
     c * r
 }
 
-fn haversine_py(_:Python,clicks:Vec<(i64,i64,f64,f64)>,historic :Vec<(i64,(Vec<f64>,Vec<f64>))>)-> PyResult<Vec<(i64,i64,Vec<usize>)>> {
+fn total_function(_:Python,clicks:Vec<(i64,i64,f64,f64,i8,i64)>,historic :Vec<(i64,(Vec<f64>,Vec<f64>))>)-> PyResult<Vec<(i64,i64,Vec<usize>)>> {
     let mut historic_hash = HashMap::new();
     historic_hash.extend(historic);
-    let out = historic_distances(clicks,historic_hash); 
+    let mut accumulator = vec![];
+    let splitted = split_vector(&clicks);
+    for same_date in splitted {
+        let out = historic_distances(&same_date,&historic_hash);
+        accumulator.push(out);
+        add_to_historic(&same_date,&mut historic_hash);
+    }
+   Ok(accumulator.into_iter().flat_map(|s| s).collect())
+}
+
+fn haversine_py(_:Python,clicks:Vec<(i64,i64,f64,f64,i8,i64)>,historic :Vec<(i64,(Vec<f64>,Vec<f64>))>)-> PyResult<Vec<(i64,i64,Vec<usize>)>> {
+    let mut historic_hash = HashMap::new();
+    historic_hash.extend(historic);
+    let out = historic_distances(&clicks,&historic_hash); 
     Ok(out)
 }
+
+fn add_to_historic(clicks: &Vec<(i64,i64,f64,f64,i8,i64)>, historic : &mut HashMap<i64,(Vec<f64>,Vec<f64>)>) {
+    for row in clicks.iter() {
+        if row.4 == 1 {
+            let person = row.0;
+            let hist_reactions = historic.entry(person).or_insert((vec![],vec![]));
+            hist_reactions.0.push(row.2);
+            hist_reactions.1.push(row.3);
+        }
+    }
+}
+
+// this is very much work in progress!!!!!
+fn split_vector(clicks: &Vec<(i64,i64,f64,f64,i8,i64)>) -> Vec<Vec<(i64,i64,f64,f64,i8,i64)>> {
+    let mut splitted = vec![]; 
+    let mut datum : i64 = 0;
+    for row in clicks{
+        if row.5 > datum {
+            datum = row.5;
+            splitted.push(vec![]);
+        }
+        splitted.last_mut().unwrap().push(row.clone());
+    }
+    return splitted
+}
+
+
 
 fn calculate_list_distance(lon:f64,lat:f64,lon_hist:&Vec<f64>,lat_hist:&Vec<f64>) -> Vec<usize> {
     let mut distances = vec![];
@@ -37,7 +77,7 @@ fn calculate_list_distance(lon:f64,lat:f64,lon_hist:&Vec<f64>,lat_hist:&Vec<f64>
     within_radius 
 }
 
-fn historic_distances(clicks:Vec<(i64,i64,f64,f64)>,historic :HashMap<i64,(Vec<f64>,Vec<f64>)>) -> Vec<(i64,i64,Vec<usize>)> {
+fn historic_distances(clicks:&Vec<(i64,i64,f64,f64,i8,i64)>,historic :&HashMap<i64,(Vec<f64>,Vec<f64>)>) -> Vec<(i64,i64,Vec<usize>)> {
     let mut distances = vec![];
     for click in clicks {
         match historic.get(&click.0) {
@@ -72,6 +112,7 @@ fn historic_distances(clicks:Vec<(i64,i64,f64,f64)>,historic :HashMap<i64,(Vec<f
 
 py_module_initializer!(libmyrustlib, initlibmyrustlib, PyInit_myrustlib, |py, m | {
     try!(m.add(py, "__doc__", "This module is implemented in Rust"));
-    m.add(py, "haversine", py_fn!(py, haversine_py(clicks:Vec<(i64,i64,f64,f64)>,historic :Vec<(i64,(Vec<f64>,Vec<f64>))>)))?;
+    m.add(py, "haversine", py_fn!(py, haversine_py(clicks:Vec<(i64,i64,f64,f64,i8,i64)>,historic :Vec<(i64,(Vec<f64>,Vec<f64>))>)))?;
+    m.add(py, "total_function", py_fn!(py, total_function(clicks:Vec<(i64,i64,f64,f64,i8,i64)>,historic :Vec<(i64,(Vec<f64>,Vec<f64>))>)))?;
     Ok(())
 });
